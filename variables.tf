39143665f2c5dce1e61b4286f07353d7e6f7c683
Luis Gustavo Bordon <luisgustavobordon@hotmail.com>
2026-03-06 16:25:28 -0300

    add networking public and private

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "project_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "profile" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "Main CIDR of the VPC"
+}
+
+variable "vpc_additional_cidrs" {
+  type        = list(string)
+  description = "List of additional VPC CIDRs"
+  default     = []
+}
+
+variable "public_subnets" {
+  description = "List of VPC Public Subnets"
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
+
+variable "private_subnets" {
+  description = "List of VPC Private Subnets"
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
